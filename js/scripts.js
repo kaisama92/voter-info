f54bd6931d2ee49ed0ed0e57a579f91135a40c96
Kai Clausen <kaiclausen123@gmail.com>
2023-01-11 14:39:25 -0800

    ad if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 window.onload = function(){
   document.querySelector("form").onsubmit = function(event){
   event.preventDefault();
-    const age = parseInt(document.querySelector("input#age-id")).value;
+    // const ageId = document.querySelector("input#age-id").value;
+    // const age = parseInt(ageId);
+    const age = parseInt(document.querySelector("input#age-id").value);
 
     console.log("age ", age);
 
@@ -9,9 +11,11 @@ window.onload = function(){
     if(age < 18) {
       console.log("in IF ");
       document.getElementById("too-young").removeAttribute("class");
+      document.getElementById("old-enough").setAttribute("class", "hidden");
     } else {
       console.log("in else ");
       document.getElementById("old-enough").removeAttribute("class");
+      document.getElementById("too-young").setAttribute("class","hidden");
     }   
   }
 }
